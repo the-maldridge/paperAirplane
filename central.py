452eda25269b78f8e9bbb97a1f2c2df53e613782
Michael Aldridge <aldridge.mac@gmail.com>
2014-07-07 22:49:20 -0500

    added basic postscript parser

--- central.py
+++ central.py
@@ -6,8 +6,11 @@ import base64
 import threading
 import database
 import time
+import Queue
 
 class IncommingJob(SocketServer.BaseRequestHandler):
+    self.qref = ""
+
     def handle(self):
         logging.debug("Processing new job from %s", self.client_address[0])
         data = self.request.recv(256)
@@ -18,9 +21,20 @@ class IncommingJob(SocketServer.BaseRequestHandler):
         self.job = json.loads(base64.b64decode(jobJSON))
         logging.info("Recieved job %s from %s on %s", self.job["name"], self.job["originUser"], self.job["originPrinter"])
 
+    def sendToBilling(self, jid):
+        self.qref.put(jid)
+
+    def saveJob(self, baseDir, job):
+        jid = job["name"]
+        os.chdir(baseDir)
+        spoolFile = open(jid, 'w')
+        json.dump(job, jid)
+        self.sendToBilling(jid)
+
 class Spooler():
-    def __init__(self, bindaddr, bindport):
+    def __init__(self, bindaddr, bindport, spooldir, qref):
         try:
+            IncommingJob.qref = qref
             logging.info("Initializing master spooler on %s:%s", bindaddr, bindport)
             server = SocketServer.TCPServer((bindaddr, bindport), IncommingJob)
             server.serve_forever()
@@ -30,19 +44,30 @@ class Spooler():
 class PSParser():
     def __init__(self):
         logging.info("Loaded PostScript Parser")
+        
+    def __getPSFromJID(self, jid):
+        jobFile = open(jid, 'r')
+        job = json.load(jobFile)
+        jobFile.close()
+        return job["postscript"]
 
-    def isDuplex(self, ps, jid):
+    def isDuplex(self, jid):
+        ps = self.__getPSFromJID(jid)
         if("/Duplex true" in ps):
             logging.debug("job %s is duplex enabled", jid)
             return True
         else:
             logging.debug("job %s is not duplex enabled", jid)
+            return False
 
     def isColor(self, ps, jid):
         logging.debug("Checking to see if %s is in color", jid)
 
-    def pageCount(self, ps, jid):
+    def pageCount(self, jid):
+        ps = self.__getPSFromJID(jid)
         logging.debug("Computing page count for %s", jid)
+        numPages = ps.count("%%Page:")
+        return numPages
 
 class Billing():
     def __init__(self, path):
@@ -50,14 +75,26 @@ class Billing():
         logging.debug("Attempting to connect to database")
         self.db = database.BillingDB(path)
         logging.debug("Successfully connected to database!")
+        logging.debug("Attempting to create a parser")
+        self.parser = PSParser()
+        logging.debug9"Successfully created parser")
+
+    def computeCost(self, jid):
+        cost = self.parser.pageCount(jid)
+        if self.parser.isDuplex(jid):
+            cost = cost / 2
+        logging.info("Billing user %s %s credit(s) for job %s", job["originUser"], cost, jid)
 
 class CentralControl():
     def __init__(self):
         logging.info("Initializing CentralControl")
 
+        self.QtoBill = Queue.Queue()
+        self.QtoPrint = Queue.Queue()
+
         self.threads = []
-        self.threads.append(threading.Thread(target=Spooler, args=("localhost", 3201)))
-        self.threads.append(threading.Thread(target=Billing, args=("test.sqlite",)))
+        self.threads.append(threading.Thread(target=Spooler, args=("localhost", 3201, self.QtoBill)))
+        self.threads.append(threading.Thread(target=Billing, args=("test.sqlite", self.QtoBill, self.QtoPrint)))
 
         logging.info("GOING POLYTHREADED")
         for thread in self.threads:
