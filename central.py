5980e8c2e847cb74986f0e76028418ab00fee418
Michael Aldridge <aldridge.mac@gmail.com>
2014-07-15 23:34:02 -0500

    major logging refactor as well as printin support

--- central.py
+++ central.py
@@ -10,17 +10,19 @@ import Queue
 import os
 
 class IncommingJob():
-    def __init__(self, con, addr, toBill):
-        self.toBill = toBill
+    def __init__(self, con, addr, toRelease):
+        self.toRelease = toRelease
         self.con = con
         self.addr = addr
+
+        self.logger = logging.getLogger("JobHandler")
 
         jobRaw = self.getJob()
         jid = self.saveJob(jobRaw)
         self.sendToBilling(jid)
 
     def getJob(self):
-        logging.debug("Processing new job from %s", self.addr[0])
+        self.logger.debug("Processing new job from %s", self.addr[0])
         self.con.settimeout(1)
         data = self.con.recv(256)
         jobJSON = data
@@ -28,12 +30,12 @@ class IncommingJob():
             data = self.con.recv(256)
             jobJSON += data
         job = json.loads(base64.b64decode(jobJSON))
-        logging.info("Recieved job %s from %s on %s", job["name"], job["originUser"], job["originPrinter"])
+        self.logger.info("Recieved job %s from %s on %s", job["name"], job["originUser"], job["originPrinter"])
         return job
 
     def sendToBilling(self, jid):
-        logging.debug("Sending %s for billing", jid)
-        self.toBill.put(jid)
+        self.logger.debug("Sending %s for job release", jid)
+        self.toRelease.put(jid)
 
     def saveJob(self, job):
         jid = job["name"]
@@ -43,37 +45,39 @@ class IncommingJob():
         return jid
 
 class Spooler():
-    def __init__(self, control, bindaddr, bindport, spooldir, toBill):
+    def __init__(self, control, bindaddr, bindport, spooldir, toRelease):
         self.threadOps = control
         self.bindaddr = bindaddr
         self.bindport = bindport
         self.spooldir = spooldir
-        self.toBill = toBill
+        self.toRelease = toRelease
+
+        self.logger = logging.getLogger("CoreSpooler")
 
         #need to improve this
         # currently it moves us into the spooler's main directory
-        logging.debug("Current path is %s", os.getcwd())
+        self.logger.debug("Current path is %s", os.getcwd())
         if(spooldir not in os.getcwd()):
             try:
-                logging.info("Pivoting to master spool directory")
+                self.logger.info("Pivoting to master spool directory")
                 os.chdir(spooldir)
-                logging.debug("Successfully found master spool directory")
+                self.logger.debug("Successfully found master spool directory")
             except OSError:
-                logging.warning("Could not use master spool directory")
-                logging.warning("Attempting to create new spool directory")
+                self.logger.warning("Could not use master spool directory")
+                self.logger.warning("Attempting to create new spool directory")
                 os.mkdir(spooldir)
                 os.chdir(spooldir)
-                logging.info("Successfully found master spool directory")
+                self.logger.info("Successfully found master spool directory")
         else:
-            logging.debug("Already in spooldir")
+            self.logger.debug("Already in spooldir")
 
         # attempt to bind the master spooler onto a port
         try:
-            logging.info("Initializing master spooler on %s:%s", bindaddr, bindport)
+            self.logger.info("Initializing master spooler on %s:%s", bindaddr, bindport)
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.s.bind((bindaddr, bindport))
         except Exception as e:
-            logging.exception("Could not bind: %s", e)
+            self.logger.exception("Could not bind: %s", e)
 
         #clear thread lock
         self.threadOps.get(False)
@@ -83,7 +87,7 @@ class Spooler():
     def listener(self):
         self.s.listen(5)
         con, addr = self.s.accept()
-        t = threading.Thread(target=IncommingJob, args=(con, addr, self.toBill))
+        t = threading.Thread(target=IncommingJob, args=(con, addr, self.toRelease))
         t.daemon = True
         t.start()
 
@@ -93,7 +97,8 @@ class Spooler():
 
 class PSParser():
     def __init__(self):
-        logging.info("Loaded PostScript Parser")
+        self.logger = logging.getLogger("PSParser")
+        self.logger.info("Loaded PostScript Parser")
         
     def __getPSFromJID(self, jid):
         jobFile = open(jid, 'r')
@@ -104,47 +109,50 @@ class PSParser():
     def isDuplex(self, jid):
         ps = self.__getPSFromJID(jid)
         if("/Duplex true" in ps):
-            logging.debug("job %s is duplex enabled", jid)
+            self.logger.debug("job %s is duplex enabled", jid)
             return True
         else:
-            logging.debug("job %s is not duplex enabled", jid)
+            self.logger.debug("job %s is not duplex enabled", jid)
             return False
 
     def pageCount(self, jid):
         ps = self.__getPSFromJID(jid)
-        logging.debug("Computing page count for %s", jid)
+        self.logger.debug("Computing page count for %s", jid)
         numPages = ps.count("%%Page:")
         return numPages
 
 class Billing():
-    def __init__(self, control, spoolDir, dbpath, toBill):
+    def __init__(self, control, spoolDir, dbpath, toBill, toPrint):
         self.threadOps = control
         self.toBill = toBill
+        self.toPrint = toPrint
+
+        self.logger = logging.getLogger("Billing")
 
         #attempt to get to the same dir the spooler is in
         #first hold until the spooler is running
-        logging.info("Billing: waiting for spooler to initialize")
+        self.logger.info("Billing: waiting for spooler to initialize")
         self.threadOps.join()
         if(spoolDir not in os.getcwd()):
             try:
                 os.chdir(spoolDir)
             except OSError as e:
-                logging.critical("Billing couldn't get to the spooler dir")
-                logging.critical("%s", e)
+                self.logger.critical("Billing couldn't get to the spooler dir")
+                self.logger.critical("%s", e)
                 self.threadOps.put("HALT")
         else:
-            logging.debug("Somehow already in the spooler's directory")
+            self.logger.debug("Somehow already in the spooler's directory")
         
         #init some internal instances of stuff
-        logging.info("Initializing Billing Manager")
-        logging.debug("Attempting to connect to database")
+        self.logger.info("Initializing Billing Manager")
+        self.logger.debug("Attempting to connect to database")
         self.db = database.BillingDB(dbpath)
-        logging.debug("Successfully connected to database")
+        self.logger.debug("Successfully connected to database")
 
-        logging.debug("Successfully connected to database!")
-        logging.debug("Attempting to create a parser")
+        self.logger.debug("Successfully connected to database!")
+        self.logger.debug("Attempting to create a parser")
         self.parser = PSParser()
-        logging.debug("Successfully created parser")
+        self.logger.debug("Successfully created parser")
 
         #enter main loop
         self.run()
@@ -154,7 +162,9 @@ class Billing():
             jid = self.toBill.get(block=True)
             cost = self.computeCost(jid)
             user = self.getUser(jid)
-            logging.info("Billing user %s %s credit(s) for job %s", user, cost, jid)
+            self.logger.info("Billing user %s %s credit(s) for job %s", user, cost, jid)
+            self.logger.debug("Forwarding %s to print manager", jid)
+            self.toPrint.put(jid)
 
     def computeCost(self, jid):
         cost = self.parser.pageCount(jid)
@@ -169,23 +179,117 @@ class Billing():
         f.close()
         return user
 
+class JobRelease():
+    def __init__(self, threadControl, spoolDir, toRelease, toBill):
+        self.threadOps = threadControl
+        self.toRelease = toRelease
+        self.toBill = toBill
+
+        self.logger = logging.getLogger("JobRelease")
+
+        #attempt to get to the same dir the spooler is in
+        #first hold until the spooler is running
+        self.logger.info("Waiting on spooler to initialize")
+        self.threadOps.join()
+        if(spoolDir not in os.getcwd()):
+            try:
+                os.chdir(spoolDir)
+            except OSError as e:
+                self.logger.critical("Couldn't get to the spooler dir")
+                self.logger.critical("%s", e)
+                self.threadOps.put("HALT")
+        else:
+            self.logger.debug("Somehow already in the spooler's directory")
+
+        self.run()
+
+    def run(self):
+        #release all jobs immeadiately, need to rewrite at some point
+        while(True):
+            jid = self.toRelease.get(block=True)
+            self.logger.debug("Forwarding job %s for billing", jid)
+            self.toBill.put(jid)
+            
+
+class SendToPrinter():
+    def __init__(self, threadControl, spoolDir, toPrint):
+        self.toPrint = toPrint
+        self.threadOps = threadControl
+
+        self.logger = logging.getLogger("PrinterOutput")
+
+        #attempt to get to the same dir the spooler is in
+        #first hold until the spooler is running
+        self.logger.info("Waiting for spooler to initialize")
+        self.threadOps.join()
+        if(spoolDir not in os.getcwd()):
+            try:
+                os.chdir(spoolDir)
+            except OSError as e:
+                self.logger.critical("Couldn't get to the spooler dir")
+                self.logger.critical("%s", e)
+                self.threadOps.put("HALT")
+        else:
+            self.logger.debug("Somehow already in the spooler's directory")
+
+        self.run()
+
+    def run(self):
+        while(True):
+            jid = self.toPrint.get(block=True)
+            self.logger.debug("Got print request for job %s", jid)
+            self.printJob(jid)
+            self.logger.debug("Printed job %s", jid)
+            self.rmJob(jid)
+
+    def printJob(self, jid):
+        #replace these with something useful
+        printer = "192.168.42.18"
+        port = 9100
+
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            ps = self.getPS(jid)
+            s.connect((printer, port))
+            s.sendall(ps)
+            s.close()
+        except Exception as e:
+            self.logger.critical("Encountered error while printing: %s", e)
+
+    def getPS(self, jid):
+        f = open(jid)
+        j = json.load(f)
+        f.close()
+        return j["postscript"]
+
+    def rmJob(self, jid):
+        self.logger.debug("Removing spool file for %s", jid)
+        os.remove(jid)
+        self.logger.info("Completed handling of %s", jid)
+
 class CentralControl():
     def __init__(self):
-        logging.info("Initializing CentralControl")
+        self.logger = logging.getLogger("CentralControl")
+
+        self.logger.info("Initializing CentralControl")
 
         self.threadControl = Queue.Queue()
         self.toBill = Queue.Queue()
+        self.toRelease = Queue.Queue()
+        self.toPrint = Queue.Queue()
 
         self.threads = []
-        self.threads.append(threading.Thread(target=Spooler, args=(self.threadControl, "localhost", 3201, "coreSpool", self.toBill)))
-        self.threads.append(threading.Thread(target=Billing, args=(self.threadControl, "coreSpool", "test.sqlite", self.toBill)))
+        self.threads.append(threading.Thread(target=Spooler, args=(self.threadControl, "localhost", 3201, "coreSpool", self.toRelease)))
+        self.threads.append(threading.Thread(target=Billing, args=(self.threadControl, "coreSpool", "test.sqlite", self.toBill, self.toPrint)))
+        self.threads.append(threading.Thread(target=JobRelease, args=(self.threadControl, "coreSpool", self.toRelease, self.toBill)))
+        self.threads.append(threading.Thread(target=SendToPrinter, args=(self.threadControl, "coreSpool", self.toPrint)))
 
         #set up startup locks before running:
         self.threadControl.put("spoolerStartup")
 
 
     def run(self):
-        logging.info("GOING POLYTHREADED")
+        self.logger.info("GOING POLYTHREADED")
         for thread in self.threads:
             thread.daemon = True
             thread.start()
@@ -197,7 +301,7 @@ class CentralControl():
             else:
                 time.sleep(1)
 
-        logging.info("All threads have exited, now exiting program")
+        self.logger.info("All threads have exited, now exiting program")
 
 if __name__ == "__main__":
     logging.basicConfig(level = logging.DEBUG)
